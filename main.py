27e6f7b512724b3baa4efaa6a459494a8f53ec7e
CSIBTN <krutinivan3@gmail.com>
2024-03-10 21:05:51 +0100

    updated main.py and added data directory

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from app.io import input
+from app.io import output
+
+
 def main():
-    pass
+    text_from_console = input.console_input()
+
+    text_from_file = input.read_from_file()
+
+    text_with_pandas = input.read_with_pandas()
+
+    output.console_output(text_from_console)
+    output.console_output(text_from_file)
+    output.console_output(text_with_pandas)
+
+    output.write_to_file(text_from_console)
+    output.write_to_file(text_from_file)
+    output.write_to_file(text_with_pandas)
 
 
 if __name__ == "__main__":
